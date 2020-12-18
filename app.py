ac0d1cc5d9da98f62804e35d1bd65ab7ae3e1f14
Josh <JMWilliams3790@gmail.com>
2020-12-17 17:52:47 -0800

    added base files, and templates folder

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_craigslist
 
-# Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
 
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
 
-# Route to render index.html template using data from Mongo
+# clear all existing data out of the collection.
+# For demo purposes only, 
+# you may not want to do this for an app you're building!
+mongo.db.listings.drop()
+
 @app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", vacation=destination_data)
+def index():
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
 
 
-# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
-
-    # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
+def scraper():
+    listings = mongo.db.listings
+    listings_data = scrape_craigslist.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
